67944eec6dd16e1093a73dc6a7ec08bfc9d26eda
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-07-19 19:44:59 +0800

    first commit

---
+++ src/testing/CanvasShellTest.java
@@ -0,0 +1,48 @@
+package testing;
+
+import java.awt.Color;
+
+import myutils.CanvasShell;
+import myutils.Colors;
+
+public class CanvasShellTest extends CanvasShell {
+	private static final long serialVersionUID = 1L;
+
+	public CanvasShellTest(int width, int height, int scale, String title, double nsPerTick,
+			double nsPerRender) {
+		super(width, height, scale, title, nsPerTick, nsPerRender);
+
+	}
+
+	@Override
+	protected void init() {
+		setBackground(Color.BLACK);
+	}
+
+	@Override
+	protected void myTick() {
+
+	}
+
+	@Override
+	protected void myRender() {
+		for (int y = 0; y < HEIGHT; y++)
+			for (int x = 0; x < WIDTH; x++) {
+				double r = 1.0 * -x / WIDTH ;
+				double g = 1.0 * y / HEIGHT;
+				double b = 1.0 * (x + y) / (WIDTH + HEIGHT);
+				r*=tickCount;
+				g*=tickCount;
+				b*=tickCount;
+				//System.out.println("r:" + r + ", g:" + g + ", b:" + b);
+				pixels[x + y * WIDTH] = (int) Colors.get(r, g, b);
+				// pixels[x + y * WIDTH] = Colors.get(1,1,1);
+			}
+	}
+
+	@Override
+	protected void myDebugInfo() {
+
+	}
+
+}

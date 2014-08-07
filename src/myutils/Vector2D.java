05aef856b4cdf08c23d57e2f76462907b3a9f0ef
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-08-07 22:32:45 +0800

    updated from particle system

--- src/myutils/Vector2D.java
+++ src/myutils/Vector2D.java
@@ -20,26 +20,47 @@ public class Vector2D {
 		y = (float) Math.sin(d);
 	}
 
+	public static Vector2D getRandom() {
+		double d = Utils.random.nextDouble() * 2 * Math.PI;
+		float x = (float) Math.cos(d);
+		float y = (float) Math.sin(d);
+		return new Vector2D(x, y);
+	}
+
 	public Vector2D clone() {
 		return new Vector2D(x, y);
 	}
 
-	public double getMagnitude() {
-		return (Math.sqrt(x * x + y * y));
+	public float getMagnitude() {
+		return (float) (Math.sqrt(x * x + y * y));
 	}
 
-	public void setMagnitude(double d) {
+	public void setMagnitude(float d) {
 		if (getMagnitude() == 0) {
 			setRandom();
+			x *= d;
+			y *= d;
 			return;
 		}
-		float r = (float) (1 / getMagnitude() * d);
+		float r = 1f / getMagnitude() * d;
 		x *= r;
 		y *= r;
 	}
 
-	public void normalize() {
-		setMagnitude(1);
+	public void min(float d) {
+		if (getMagnitude() < d)
+			setMagnitude(d);
+	}
+
+	public void max(float d) {
+		if (getMagnitude() > d)
+			setMagnitude(d);
+	}
+
+	public Vector2D normalize() {
+		Vector2D result = this.clone();
+		result.setMagnitude(1);
+		return result;
 	}
 
 	public void plus(Vector2D pv) {
@@ -50,6 +71,16 @@ public class Vector2D {
 	public void minus(Vector2D pv) {
 		x -= pv.x;
 		y -= pv.y;
+	}
+
+	public void multiply(Vector2D pv) {
+		x *= pv.x;
+		y *= pv.y;
+	}
+
+	public void multiply(float r) {
+		x *= r;
+		y *= r;
 	}
 
 	public static Vector2D add(Vector2D p1, Vector2D p2) {

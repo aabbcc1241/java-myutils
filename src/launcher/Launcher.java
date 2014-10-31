0317e2098ed0a1c94e6488db09b6a57cd38e2f60
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-11-01 00:00:06 +0800

    updated package conversion, introduced launcher

---
+++ src/launcher/Launcher.java
@@ -0,0 +1,16 @@
+package launcher;
+
+public class Launcher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("main start");
+
+		/** call my class **/
+
+		System.out.println("main end");
+	}
+
+}

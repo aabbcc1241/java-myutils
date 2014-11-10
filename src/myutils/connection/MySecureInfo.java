f33614f948af0c1aa755a313edd912c3e1fe915d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2014-11-10 13:22:36 +0800

    added sql file support

--- src/myutils/connection/MySecureInfo.java
+++ src/myutils/connection/MySecureInfo.java
@@ -5,6 +5,7 @@ package myutils.connection;
  */
 public class MySecureInfo {
 
+	// digit ocean server
 	private static final String sshHost = "128.199.172.14";
 	private static final String sshUsername = "beeno";
 	private static final String sshPassword = "wpc1415";
@@ -13,14 +14,16 @@ public class MySecureInfo {
 	private static final int portforwardRemotePort = 3306;
 	private static final String mysqlProtocol = "jdbc:mariadb";// jdbc:mysql
 	private static final String mysqlHost = "localhost";
-	// private static final int mysqlPort = portforwardLocalPort;
-	// private static final String mysqlUsername = "beeno";
-	// private static final String mysqlPassword = "wpc1415";
-	// private static final String mysqlDatabasename = "beeno"; // wholesaler
-	private static final int mysqlPort = 3306;
-	private static final String mysqlUsername = "root";
-	private static final String mysqlPassword = "mysqlB(10v2TC";
-	private static final String mysqlDatabasename = "nndb"; // wholesaler
+	private static final int mysqlPort = portforwardLocalPort;
+	private static final String mysqlUsername = "beeno";
+	private static final String mysqlPassword = "wpc1415";
+	private static final String mysqlDatabasename = "beeno";
+
+	// local server
+	// private static final int mysqlPort = 3306;
+	// private static final String mysqlUsername = "root";
+	// private static final String mysqlPassword = "mysqlB(10v2TC";
+	// private static final String mysqlDatabasename = "nndb"; // wholesaler
 
 	/**
 	 * @return my secure info @

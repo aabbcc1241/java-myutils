97557bcae960f981a63f4a6451dfa7508c48d237
Beeno Tung <aabbcc1241@yahoo.com.hk>
2014-12-03 21:21:37 +0800

    fixed merge conflit on CircleLayout (changed CircleLayout into public class); updated formatting

--- src/webs/layout/CenterLayout.java
+++ src/webs/layout/CenterLayout.java
@@ -43,150 +43,137 @@ package webs.layout;
  *
  */
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Insets;
-import java.awt.LayoutManager;
+import java.awt.*;
 import java.io.Serializable;
 
 /**
  * A layout manager that displays a single component in the center of its
  * container.
- * 
+ *
  * @author David Gilbert
  */
 public class CenterLayout implements LayoutManager, Serializable {
 
-	/** For serialization. */
-	private static final long serialVersionUID = 469319532333015042L;
+    /**
+     * For serialization.
+     */
+    private static final long serialVersionUID = 469319532333015042L;
 
-	/**
-	 * Creates a new layout manager.
-	 */
-	public CenterLayout() {
-	}
+    /**
+     * Creates a new layout manager.
+     */
+    public CenterLayout() {
+    }
 
-	/**
-	 * Returns the preferred size.
-	 * 
-	 * @param parent
-	 *            the parent.
-	 * 
-	 * @return the preferred size.
-	 */
-	public Dimension preferredLayoutSize(final Container parent) {
+    /**
+     * Returns the preferred size.
+     *
+     * @param parent the parent.
+     * @return the preferred size.
+     */
+    public Dimension preferredLayoutSize(final Container parent) {
 
-		synchronized (parent.getTreeLock()) {
-			final Insets insets = parent.getInsets();
-			if (parent.getComponentCount() > 0) {
-				final Component component = parent.getComponent(0);
-				final Dimension d = component.getPreferredSize();
-				return new Dimension((int) d.getWidth() + insets.left
-						+ insets.right, (int) d.getHeight() + insets.top
-						+ insets.bottom);
-			} else {
-				return new Dimension(insets.left + insets.right, insets.top
-						+ insets.bottom);
-			}
-		}
+        synchronized (parent.getTreeLock()) {
+            final Insets insets = parent.getInsets();
+            if (parent.getComponentCount() > 0) {
+                final Component component = parent.getComponent(0);
+                final Dimension d = component.getPreferredSize();
+                return new Dimension((int) d.getWidth() + insets.left
+                        + insets.right, (int) d.getHeight() + insets.top
+                        + insets.bottom);
+            } else {
+                return new Dimension(insets.left + insets.right, insets.top
+                        + insets.bottom);
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Returns the minimum size.
-	 * 
-	 * @param parent
-	 *            the parent.
-	 * 
-	 * @return the minimum size.
-	 */
-	public Dimension minimumLayoutSize(final Container parent) {
+    /**
+     * Returns the minimum size.
+     *
+     * @param parent the parent.
+     * @return the minimum size.
+     */
+    public Dimension minimumLayoutSize(final Container parent) {
 
-		synchronized (parent.getTreeLock()) {
-			final Insets insets = parent.getInsets();
-			if (parent.getComponentCount() > 0) {
-				final Component component = parent.getComponent(0);
-				final Dimension d = component.getMinimumSize();
-				return new Dimension(d.width + insets.left + insets.right,
-						d.height + insets.top + insets.bottom);
-			} else {
-				return new Dimension(insets.left + insets.right, insets.top
-						+ insets.bottom);
-			}
-		}
+        synchronized (parent.getTreeLock()) {
+            final Insets insets = parent.getInsets();
+            if (parent.getComponentCount() > 0) {
+                final Component component = parent.getComponent(0);
+                final Dimension d = component.getMinimumSize();
+                return new Dimension(d.width + insets.left + insets.right,
+                        d.height + insets.top + insets.bottom);
+            } else {
+                return new Dimension(insets.left + insets.right, insets.top
+                        + insets.bottom);
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Lays out the components.
-	 * 
-	 * @param parent
-	 *            the parent.
-	 */
-	public void layoutContainer(final Container parent) {
+    /**
+     * Lays out the components.
+     *
+     * @param parent the parent.
+     */
+    public void layoutContainer(final Container parent) {
 
-		synchronized (parent.getTreeLock()) {
-			if (parent.getComponentCount() > 0) {
-				final Insets insets = parent.getInsets();
-				final Dimension parentSize = parent.getSize();
-				final Component component = parent.getComponent(0);
-				final Dimension componentSize = component.getPreferredSize();
-				final int xx = insets.left
-						+ (Math.max((parentSize.width - insets.left
-								- insets.right - componentSize.width) / 2, 0));
-				final int yy = insets.top
-						+ (Math.max((parentSize.height - insets.top
-								- insets.bottom - componentSize.height) / 2, 0));
-				component.setBounds(xx, yy, componentSize.width,
-						componentSize.height);
-			}
-		}
+        synchronized (parent.getTreeLock()) {
+            if (parent.getComponentCount() > 0) {
+                final Insets insets = parent.getInsets();
+                final Dimension parentSize = parent.getSize();
+                final Component component = parent.getComponent(0);
+                final Dimension componentSize = component.getPreferredSize();
+                final int xx = insets.left
+                        + (Math.max((parentSize.width - insets.left
+                        - insets.right - componentSize.width) / 2, 0));
+                final int yy = insets.top
+                        + (Math.max((parentSize.height - insets.top
+                        - insets.bottom - componentSize.height) / 2, 0));
+                component.setBounds(xx, yy, componentSize.width,
+                        componentSize.height);
+            }
+        }
 
-	}
+    }
 
-	/**
-	 * Not used.
-	 * 
-	 * @param comp
-	 *            the component.
-	 */
-	public void addLayoutComponent(final Component comp) {
-		// not used.
-	}
+    /**
+     * Not used.
+     *
+     * @param comp the component.
+     */
+    public void addLayoutComponent(final Component comp) {
+        // not used.
+    }
 
-	/**
-	 * Not used.
-	 * 
-	 * @param comp
-	 *            the component.
-	 */
-	public void removeLayoutComponent(final Component comp) {
-		// not used
-	}
+    /**
+     * Not used.
+     *
+     * @param comp the component.
+     */
+    public void removeLayoutComponent(final Component comp) {
+        // not used
+    }
 
-	/**
-	 * Not used.
-	 * 
-	 * @param name
-	 *            the component name.
-	 * @param comp
-	 *            the component.
-	 */
-	public void addLayoutComponent(final String name, final Component comp) {
-		// not used
-	}
+    /**
+     * Not used.
+     *
+     * @param name the component name.
+     * @param comp the component.
+     */
+    public void addLayoutComponent(final String name, final Component comp) {
+        // not used
+    }
 
-	/**
-	 * Not used.
-	 * 
-	 * @param name
-	 *            the component name.
-	 * @param comp
-	 *            the component.
-	 */
-	public void removeLayoutComponent(final String name, final Component comp) {
-		// not used
-	}
+    /**
+     * Not used.
+     *
+     * @param name the component name.
+     * @param comp the component.
+     */
+    public void removeLayoutComponent(final String name, final Component comp) {
+        // not used
+    }
 
 }
--- src/webs/layout/CenterLayout_lobochief.java
+++ src/webs/layout/CenterLayout_lobochief.java
@@ -24,95 +24,92 @@ package webs.layout;
  * Created on Mar 19, 2005
  */
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.LayoutManager;
+import java.awt.*;
 
 /**
  * @author J. H. S.
  */
 public class CenterLayout_lobochief implements LayoutManager {
-	public void addLayoutComponent(String arg0, Component arg1) {
-	}
+    public void addLayoutComponent(String arg0, Component arg1) {
+    }
 
-	public void removeLayoutComponent(Component arg0) {
-	}
+    public void removeLayoutComponent(Component arg0) {
+    }
 
-	public Dimension preferredLayoutSize(Container arg0) {
-		java.awt.Insets insets = arg0.getInsets();
-		int count = arg0.getComponentCount();
-		if (count > 0) {
-			Dimension d = arg0.getComponent(0).getPreferredSize();
-			return new Dimension(d.width + insets.left + insets.right, d.height
-					+ insets.top + insets.bottom);
-		} else {
-			return new Dimension(insets.left + insets.right, insets.top
-					+ insets.bottom);
-		}
-	}
+    public Dimension preferredLayoutSize(Container arg0) {
+        java.awt.Insets insets = arg0.getInsets();
+        int count = arg0.getComponentCount();
+        if (count > 0) {
+            Dimension d = arg0.getComponent(0).getPreferredSize();
+            return new Dimension(d.width + insets.left + insets.right, d.height
+                    + insets.top + insets.bottom);
+        } else {
+            return new Dimension(insets.left + insets.right, insets.top
+                    + insets.bottom);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.awt.LayoutManager#minimumLayoutSize(java.awt.Container)
-	 */
-	public Dimension minimumLayoutSize(Container arg0) {
-		java.awt.Insets insets = arg0.getInsets();
-		int count = arg0.getComponentCount();
-		if (count > 0) {
-			Dimension d = arg0.getComponent(0).getMinimumSize();
-			return new Dimension(d.width + insets.left + insets.right, d.height
-					+ insets.top + insets.bottom);
-		} else {
-			return new Dimension(insets.left + insets.right, insets.top
-					+ insets.bottom);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.awt.LayoutManager#minimumLayoutSize(java.awt.Container)
+     */
+    public Dimension minimumLayoutSize(Container arg0) {
+        java.awt.Insets insets = arg0.getInsets();
+        int count = arg0.getComponentCount();
+        if (count > 0) {
+            Dimension d = arg0.getComponent(0).getMinimumSize();
+            return new Dimension(d.width + insets.left + insets.right, d.height
+                    + insets.top + insets.bottom);
+        } else {
+            return new Dimension(insets.left + insets.right, insets.top
+                    + insets.bottom);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.awt.LayoutManager#layoutContainer(java.awt.Container)
-	 */
-	public void layoutContainer(Container container) {
-		int count = container.getComponentCount();
-		if (count > 0) {
-			Component child = container.getComponent(0);
-			java.awt.Insets insets = container.getInsets();
-			int availWidth = container.getWidth() - insets.left - insets.right;
-			int availHeight = container.getHeight() - insets.top
-					- insets.bottom;
-			Dimension preferredSize = child.getPreferredSize();
-			double preferredWidth = preferredSize.getWidth();
-			double preferredHeight = preferredSize.getHeight();
-			int width;
-			int height;
-			int x;
-			int y;
-			if (preferredWidth < availWidth) {
-				x = (int) Math.round(insets.left
-						+ (availWidth - preferredWidth) / 2);
-				width = (int) Math.round(preferredWidth);
-			} else {
-				x = insets.left;
-				width = availWidth;
-			}
-			if (preferredHeight < availHeight) {
-				y = (int) Math.round(insets.top
-						+ (availHeight - preferredHeight) / 2);
-				height = (int) Math.round(preferredHeight);
-			} else {
-				y = insets.top;
-				height = availHeight;
-			}
-			child.setBounds(x, y, width, height);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.awt.LayoutManager#layoutContainer(java.awt.Container)
+     */
+    public void layoutContainer(Container container) {
+        int count = container.getComponentCount();
+        if (count > 0) {
+            Component child = container.getComponent(0);
+            java.awt.Insets insets = container.getInsets();
+            int availWidth = container.getWidth() - insets.left - insets.right;
+            int availHeight = container.getHeight() - insets.top
+                    - insets.bottom;
+            Dimension preferredSize = child.getPreferredSize();
+            double preferredWidth = preferredSize.getWidth();
+            double preferredHeight = preferredSize.getHeight();
+            int width;
+            int height;
+            int x;
+            int y;
+            if (preferredWidth < availWidth) {
+                x = (int) Math.round(insets.left
+                        + (availWidth - preferredWidth) / 2);
+                width = (int) Math.round(preferredWidth);
+            } else {
+                x = insets.left;
+                width = availWidth;
+            }
+            if (preferredHeight < availHeight) {
+                y = (int) Math.round(insets.top
+                        + (availHeight - preferredHeight) / 2);
+                height = (int) Math.round(preferredHeight);
+            } else {
+                y = insets.top;
+                height = availHeight;
+            }
+            child.setBounds(x, y, width, height);
+        }
+    }
 
-	private static CenterLayout_lobochief instance = new CenterLayout_lobochief();
+    private static CenterLayout_lobochief instance = new CenterLayout_lobochief();
 
-	public static CenterLayout_lobochief getInstance() {
-		return instance;
-	}
+    public static CenterLayout_lobochief getInstance() {
+        return instance;
+    }
 }

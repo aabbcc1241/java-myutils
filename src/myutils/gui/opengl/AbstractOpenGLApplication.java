1454f88e4cb6dc23176a66e6ba084d18161083b0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-01-07 16:53:41 +0800

    hotissue: rendered image doesnt move when scrolling

--- src/myutils/gui/opengl/AbstractOpenGLApplication.java
+++ src/myutils/gui/opengl/AbstractOpenGLApplication.java
@@ -8,8 +8,7 @@ import org.lwjgl.system.MemoryUtil;
 import java.nio.ByteBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.GL_TRUE;
-import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.*;
 
 public abstract class AbstractOpenGLApplication implements Runnable {
     protected final String WINDOW_TITLE = "My OpenGL Application";
@@ -21,7 +20,7 @@ public abstract class AbstractOpenGLApplication implements Runnable {
     protected long window;
     protected float nsPerTick;
     protected float nsPerRender;
-    protected boolean ticking;
+    protected boolean ticking, rendering;
     private float deltaTick = 0;
     private float deltaRender = 0;
 
@@ -29,24 +28,32 @@ public abstract class AbstractOpenGLApplication implements Runnable {
         nsPerRender = 1e9f / 60f;
         nsPerTick = 1e9f / 30f;
         ticking = false;
+        rendering = false;
     }
 
     public void init() {
         glfwSetErrorCallback(glfwErrorCallback = Callbacks.errorCallbackPrint(System.err));
-        if (glfwInit() != GL11.GL_TRUE) throw new IllegalStateException("Failed to int GLFW");
+        if (glfwInit() != GL11.GL_TRUE)
+            throw new IllegalStateException("Failed to int GLFW");
+
         GLFW.glfwDefaultWindowHints();
         GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GL11.GL_FALSE);
+
         if ((window = GLFW.glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, MemoryUtil.NULL, MemoryUtil.NULL)) == MemoryUtil.NULL)
             throw new RuntimeException("Failed to create GLFW window");
+
         GLFW.glfwSetKeyCallback(window, glfwKeyCallback = getGLFWKeyCallback());
+
         ByteBuffer videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
         WINDOW_CX = (GLFWvidmode.width(videoMode) - WINDOW_WIDTH) / 2;
         WINDOW_CY = (GLFWvidmode.height(videoMode) - WINDOW_HEIGHT) / 2;
         GLFW.glfwSetWindowPos(window, WINDOW_CX, WINDOW_CY);
+
         GLFW.glfwMakeContextCurrent(window);
         GLFW.glfwSwapInterval(1);
         GLFW.glfwShowWindow(window);
+
         myInit();
     }
 
@@ -68,14 +75,17 @@ public abstract class AbstractOpenGLApplication implements Runnable {
 
     public void loop() {
         GLContext.createFromCurrent();
+
         float r, g, b, a;
         r = g = b = a = 0.0f;
         GL11.glClearColor(r, g, b, a);
+
         long lastTime = System.nanoTime();
         long debugTime = System.currentTimeMillis();
         long currentTime;
         deltaTick = 0;
         deltaRender = 0;
+
         while (glfwWindowShouldClose(window) == GL11.GL_FALSE) {
             currentTime = System.nanoTime();
             deltaTick += (currentTime - lastTime) / nsPerTick;
@@ -95,10 +105,10 @@ public abstract class AbstractOpenGLApplication implements Runnable {
         }
     }
 
-    protected void debugInfo() {
-    }
+    protected abstract void debugInfo();
 
     protected void tick() {
+        if (rendering) return;
         ticking = true;
         glfwPollEvents();
         myTick();
@@ -109,10 +119,16 @@ public abstract class AbstractOpenGLApplication implements Runnable {
 
     protected void render() {
         if (ticking) return;
-        glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        rendering = true;
+        reshape();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         myRender();
         glfwSwapBuffers(window);
+        rendering = false;
     }
+
+    protected abstract void reshape();
+
 
     protected abstract void myRender();
 
--- src/myutils/gui/opengl/AbstractSimpleOpenGLApplication.java
+++ src/myutils/gui/opengl/AbstractSimpleOpenGLApplication.java
@@ -11,21 +11,70 @@ import static org.lwjgl.opengl.GL11.*;
  */
 
 public abstract class AbstractSimpleOpenGLApplication extends AbstractOpenGLApplication {
+    protected float DEFAULT_SCROLL_SPEED = 0.2f;
+    protected float xRange, yRange, zRange;
+    protected float cx, cy, cz;
+    protected float vx, vy, vz;
+
     @Override
     protected GLFWKeyCallback getGLFWKeyCallback() {
         return new GLFWKeyCallback() {
             @Override
             public void invoke(long window, int key, int scanCode, int action, int mode) {
-                switch (key) {
-                    case GLFW_KEY_ESCAPE:
-                        key_escape();
-                        break;
-                    default:
-                        myKeyInvoke(window, key, scanCode, action, mode);
-                        break;
-                }
+                keyInvoke(window, key, scanCode, action, mode);
             }
         };
+    }
+
+    protected void keyInvoke(long window, int key, int scanCode, int action, int mode) {
+        switch (key) {
+            case GLFW_KEY_ESCAPE:
+                key_escape();
+                break;
+            case GLFW_KEY_W:
+                switch (action) {
+                    case GLFW_PRESS:
+                        vy = -DEFAULT_SCROLL_SPEED;
+                        break;
+                    case GLFW_RELEASE:
+                        vy = 0;
+                        break;
+                }
+                break;
+            case GLFW_KEY_S:
+                switch (action) {
+                    case GLFW_PRESS:
+                        vy = DEFAULT_SCROLL_SPEED;
+                        break;
+                    case GLFW_RELEASE:
+                        vy = 0;
+                        break;
+                }
+                break;
+            case GLFW_KEY_A:
+                switch (action) {
+                    case GLFW_PRESS:
+                        vx = -DEFAULT_SCROLL_SPEED;
+                        break;
+                    case GLFW_RELEASE:
+                        vx = 0;
+                        break;
+                }
+                break;
+            case GLFW_KEY_D:
+                switch (action) {
+                    case GLFW_PRESS:
+                        vx = DEFAULT_SCROLL_SPEED;
+                        break;
+                    case GLFW_RELEASE:
+                        vx = 0;
+                        break;
+                }
+                break;
+            default:
+                myKeyInvoke(window, key, scanCode, action, mode);
+                break;
+        }
     }
 
     protected void key_escape() {
@@ -33,4 +82,33 @@ public abstract class AbstractSimpleOpenGLApplication extends AbstractOpenGLAppl
     }
 
     protected abstract void myKeyInvoke(long window, int key, int scanCode, int action, int mode);
+
+    @Override
+    protected void myTick() {
+        cx += vx;
+        cy += vy;
+        cz += vz;
+    }
+
+    @Override
+    protected void reshape() {
+        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        glMatrixMode(GL_PROJECTION);
+        if (WINDOW_WIDTH > WINDOW_HEIGHT)
+            glOrtho(
+                    (cx - xRange) * WINDOW_WIDTH / WINDOW_HEIGHT,
+                    (cx + xRange) * WINDOW_WIDTH / WINDOW_HEIGHT,
+                    cy - yRange, cy + yRange,
+                    cz - zRange, cz + zRange
+            );
+        else
+            glOrtho(
+                    cx - xRange, cx + xRange,
+                    (cy - yRange) * WINDOW_HEIGHT / WINDOW_WIDTH,
+                    (cy + yRange) * WINDOW_HEIGHT / WINDOW_WIDTH,
+                    cz - zRange, cz + zRange
+            );
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+    }
 }

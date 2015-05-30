9f27dde5367eec634a1e0da8d34db79356d904f3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-05-30 22:35:20 +0800

    moved Timer and Worker into package lang

---
+++ src/scala/myutils/lang/Timer.scala
@@ -0,0 +1,26 @@
+package myutils.lang
+
+/**
+ * Created by beenotung on 4/9/15.
+ */
+object Timer {
+  def setTimeInterval(callback: => Unit, shouldDo: => Boolean, time: Long) {
+    Worker.forkAndStart({
+      while (shouldDo) {
+        callback
+        sleep(time)
+      }
+    })
+  }
+
+  def setTimeDelay(callback: => Unit, time: Long) {
+    Worker.forkAndStart({
+      sleep(time)
+      callback
+    })
+  }
+
+  def sleep(time: Long) = {
+    Thread sleep time
+  }
+}
